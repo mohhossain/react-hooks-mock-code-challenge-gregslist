59a4b1a52981e389745bae6f9e45f006c6945689
Mohammad <moh.hossain98@gmail.com>
2022-09-27 16:24:19 -0400

    delete in the listingContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((response) => response.json())
+      .then((items) => {
+        console.log(items);
+        setItems(items);
+      });
+  }, []);
+
+  const updateItems = (items) => {
+    setItems(items);
+  };
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer updateItems={updateItems} items={items} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,33 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import { React, useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ items, updateItems }) {
+  console.log(items, "Coming from ListingsContainer");
+
+  const [item, setItem] = useState({});
+
+  const onDelete = (item) => {
+    console.log(item, "this is coming from the listing card");
+    setItem(item);
+    const filteredItems = items.filter((data) => {
+      return data.id !== item.id;
+    });
+
+    updateItems(filteredItems);
+  };
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {items.map((item) => {
+          return (
+            <ListingCard
+              key={item.id}
+              item={item}
+              onDelete={onDelete}
+            ></ListingCard>
+          );
+        })}
       </ul>
     </main>
   );
